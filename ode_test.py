eefa98d6668f022f0dd01396bf14fdaef57cf8ec
kurt <kpdudek@bu.edu>
2018-07-09 14:54:54 -0400

    Added Van Der Pol

--- ode_test.py
+++ ode_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #coding: utf-8
 
+#Ignoring warnings that arise due to scipy being built over the new numpy
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
@@ -11,24 +12,33 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 
-def van_der_Pol(y,t):
+def exponential(y,t):
 	dydt = 2*t #np.matrix([y[1]],[(1-y[0]^2)*y[1]-y[0]])
 	return dydt
 
 t = np.linspace(0,5,100)
 y0 = [0]
-sol = odeint(van_der_Pol,y0,t)
+sol = odeint(exponential,y0,t)
 
 #print(sol)
+plt.figure(1)
 plt.plot(t,sol)
+
+
+
+
+
+def van_der_Pol(y,t):
+	dydt = [y[1],(1-y[0]**2)*y[1]-y[0]]
+	return dydt
+
+y1 = [2,0]
+t1 = np.linspace(0,20,100)
+sol1 = odeint(van_der_Pol,y1,t1)
+
+plt.figure(2)
+plt.plot(t1,sol1)
 plt.show()
-
-
-
-
-
-
-
 
 
 
